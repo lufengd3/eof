195a1909df016c58ea29bb16e1d24f00935e4730
lufeng <lufengd3@gmail.com>
2016-04-18 17:39:11 +0800

    eof.js init

---
+++ docs/blog/The ABCs of Web Components.md
@@ -0,0 +1,9 @@
+### Web Components
+- Shadow DOM
+- Custom Element
+- Template
+- HTML Imports
+
+彼此独立，可单独使用
+
+### 使用Web Components开发通用前端组件
--- src/eof.js
+++
@@ -1,9 +0,0 @@
-class Eof {
-    constructor() {
-        this.version = '0.0.1';
-    }
-
-    hello(name = 'world') {
-        console.log(`Hello ${name}`);
-    }
-}
